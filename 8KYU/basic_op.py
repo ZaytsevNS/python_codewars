3f1376678b0a7ac898c4308daa158ac917064c11
ZaytsevNS <fatalbest@mail.ru>
2021-02-17 17:20:01 +0300

    Solution for task: Basic Mathematical Operations.

---
+++ 8KYU/basic_op.py
@@ -0,0 +1,9 @@
+def basic_op(operator, value1, value2):
+    if operator == '+':
+        return  value1 + value2
+    elif operator == '-':
+        return  value1 - value2
+    elif operator == '*':
+        return  value1 * value2
+    else:
+        return  value1 / value2
