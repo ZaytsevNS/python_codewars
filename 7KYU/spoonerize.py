c95a0d0c5d27fe7d32c9d5a0bc8f1f5bfe93437e
ZaytsevNS <fatalbest@mail.ru>
2021-02-23 18:28:22 +0300

    Solution for task: Spoonerize Me.

---
+++ 7KYU/spoonerize.py
@@ -0,0 +1,13 @@
+def spoonerize(words: str) -> str:
+    """
+    This function returns a spoonerism: it's a spoken phrase 
+    in which the first letters of two of the words are swapped around.
+    """
+    if len(words.split()) == 2:
+        return words.split()[1][0] + words.split()[0][1:] + ' ' + words.split()[0][0] + words.split()[1][1:]
+    elif len(words.split()) == 3:
+        return words.split()[2][0] + words.split()[0][1:] + ' ' + words.split()[1] + ' ' + words.split()[0][1] + words.split()[2][1:]
+    else:
+        return words
+    
+spoonerize('pop corn')
