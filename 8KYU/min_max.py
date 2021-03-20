faa129d1a167ba96f64a793fcec7c09eb71a5d68
ZaytsevNS <fatalbest@mail.ru>
2021-03-20 12:13:01 +0300

    Solution for task: Find Maximum and Minimum Values of a List.

---
+++ 8KYU/min_max.py
@@ -0,0 +1,8 @@
+from typing import List, Union
+
+
+def minimum(arr: List[(Union[int, float])]) -> Union[int, float]:
+    return min(arr)
+
+def maximum(arr: List[(Union[int, float])]) -> Union[int, float]:
+    return max(arr)
