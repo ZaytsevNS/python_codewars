039060cfbcc68d5c338c30757dec79b7cfdeb0f6
ZaytsevNS <fatalbest@mail.ru>
2021-04-05 20:13:47 +0300

    Solution for task: Price of Mangoes.

---
+++ 8KYU/mango.py
@@ -0,0 +1,13 @@
+from typing import Union
+
+
+def mango(quantity: int, price: Union[int, float]) -> Union[int, float]:
+    mangoes_quantity: int = quantity
+    give_free: int = 0
+    while mangoes_quantity != 0:
+        if mangoes_quantity >= 3:
+            mangoes_quantity -= 3
+            give_free += 1
+        else:
+            break
+    return (quantity - give_free) * price
