f6edfb52ed4ee2fdf4257225665325d28f4de84a
ZaytsevNS <fatalbest@mail.ru>
2021-06-27 12:33:15 +0300

    Solution for task: Which triangle is that?

---
+++ 7KYU/type_of_triangle.py
@@ -0,0 +1,14 @@
+def type_of_triangle(a, b, c) -> str: 
+    try:
+        if (type(a) == type(b) == type(c) == int) and (c < (a + b) and b < (a + c) and a < (b + c)):
+            if a == b == c:
+	            return "Equilateral"
+            elif a == b or b == c or a == c:
+	            return "Isosceles"
+            else:
+	            return "Scalene"
+        else:
+            return "Not a valid triangle"
+    except TypeError:
+        return "Not a valid triangle"
+    
