873f3026dbdd28227fa58a67b3451453b4a38527
ZaytsevNS <fatalbest@mail.ru>
2021-07-04 17:46:19 +0300

    Fix test file.

--- 4KYU/next_bigger/test.py
+++ 4KYU/next_bigger/test.py
@@ -1,19 +1,19 @@
 import unittest
-import next_bigger as nb
+import main
 
 
 class TestNextBigger(unittest.TestCase):
     def test_one(self):
         """ Should return next bigger """
-        self.assertEqual(21, nb.next_bigger(12))
-        self.assertEqual(531, nb.next_bigger(513))
-        self.assertEqual(2071, nb.next_bigger(2017))
+        self.assertEqual(21, main.next_bigger(12))
+        self.assertEqual(531, main.next_bigger(513))
+        self.assertEqual(2071, main.next_bigger(2017))
 
     def test_two(self):
         """ Should return -1 """
-        self.assertEqual(-1, nb.next_bigger(9))
-        self.assertEqual(-1, nb.next_bigger(111))
-        self.assertEqual(-1, nb.next_bigger(531))
+        self.assertEqual(-1, main.next_bigger(9))
+        self.assertEqual(-1, main.next_bigger(111))
+        self.assertEqual(-1, main.next_bigger(531))
 
 if __name__ == '__main__':
     unittest.main()
