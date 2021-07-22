b2a752b446cb4224955a86f5988e336dee1a9651
ZaytsevNS <fatalbest@mail.ru>
2021-07-22 16:31:02 +0300

    Solution for task: Grouped by commas.

---
+++ 6KYU/group_by_commas.py
@@ -0,0 +1,4 @@
+def group_by_commas(n: int) -> str:
+    """ This function returns a string that is the decimal representation
+    of the number grouped by commas after every 3 digits """
+    return f'{n:,}'
