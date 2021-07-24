5e890db94cb795f4b681dc6fc3c94c580674db08
ZaytsevNS <fatalbest@mail.ru>
2021-07-24 11:33:59 +0300

    Solution for task: Square Every Digit.

--- 7KYU/square_digits.py
+++ 7KYU/square_digits.py
@@ -1,6 +1,2 @@
-def square_digits(num):
-    str_numbers = str(num)
-    for i in str_numbers:
-        my_array = [int(i) ** 2 for i in str_numbers]
-        z = ''.join(map(str, my_array))
-    return (int(z))
+def square_digits(num: int) -> int:
+    return int(''.join(map(str, [int(i) ** 2 for i in str(num)])))
