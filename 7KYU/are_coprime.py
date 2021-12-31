3ad0c9bc2c5cc643896bac2792b56c5be7791353
ZaytsevNS <fatalbest@mail.ru>
2021-12-31 14:14:06 +0300

    Solution for task: Coprime Validator.

---
+++ 7KYU/are_coprime.py
@@ -0,0 +1,4 @@
+def are_coprime(n: int, m: int) -> bool:
+    factors_n: set = set(i for i in range(1, n+1) if not n % i)
+    factors_m: set = set(i for i in range(1, m+1) if not m % i)
+    return True if len(factors_n & factors_m) == 1 else False
