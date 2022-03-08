e9aa5c92f7af97e62322b5e8e19501e3a93a6b76
ZaytsevNS <fatalbest@mail.ru>
2022-03-08 12:08:24 +0300

    Solution for task: Dot Calculator.

---
+++ 7KYU/calculator.py
@@ -0,0 +1,10 @@
+import operator
+
+
+def calculator(txt: str) -> str:
+    split_txt: list = txt.split()
+    operations: dict = {'+': operator.add(len(split_txt[0]), len(split_txt[2])),
+                        '-': operator.sub(len(split_txt[0]), len(split_txt[2])),
+                        '*': operator.mul(len(split_txt[0]), len(split_txt[2])),
+                        '//': operator.floordiv(len(split_txt[0]), len(split_txt[2]))}
+    return operations.get(split_txt[1]) * '.'
