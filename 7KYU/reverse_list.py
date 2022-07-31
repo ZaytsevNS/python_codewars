fec0408279b11b72c3b3172d834c116c57358e92
ZaytsevNS <fatalbest@mail.ru>
2022-07-31 20:04:39 +0300

    Solution for task: Reverse list.

---
+++ 7KYU/reverse_list.py
@@ -0,0 +1,6 @@
+# Solution for task: https://www.codewars.com/kata/57a04da9e298a7ee43000111/
+
+def reverse_list(lst: list) -> list:
+    """ This function simply reverses lists. """
+    
+    return lst[::-1]
