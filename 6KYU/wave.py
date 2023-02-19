e03cba98555cd40c4b4c6d4da7db074f668b1976
ZaytsevNS <fatalbest@mail.ru>
2023-02-19 18:21:18 +0300

    Solution for task: Mexican Wave.

---
+++ 6KYU/wave.py
@@ -0,0 +1,10 @@
+# Solution for task: https://www.codewars.com/kata/58f5c63f1e26ecda7e000029/
+
+def wave(people: str) -> list:
+    """ This function turns a string into a Mexican Wave """
+    wave: list = []
+    for i in range(len(people)):
+        if people[i] == ' ':
+            continue
+        wave.append(people[:i] + people[i].upper() + people[i+1:])
+    return wave
