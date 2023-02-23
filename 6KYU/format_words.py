f87b7dce0755dd2a34b122c3ae0bca440502ac48
ZaytsevNS <fatalbest@mail.ru>
2023-02-23 12:48:14 +0300

    Solution for task: Format words into a sentence.

--- 6KYU/format_words.py
+++ 6KYU/format_words.py
@@ -1,7 +1,7 @@
 # Solution for task: https://www.codewars.com/kata/51689e27fe9a00b126000004/
 
 def format_words(words) -> str:
-    if (words == [""]) or (words is None):
+    if (words == [""]) or (words is None) or (len(words) == 0):
         return ""
     elif len(words) > 0:
         filter_words: list = list(filter(lambda i: len(i) > 0, words))
@@ -9,5 +9,3 @@ def format_words(words) -> str:
             return filter_words[0]
         elif len(filter_words) > 1:
             return f"{', '.join(i for i in filter_words[:-1])} and {filter_words[-1]}"
-    else:
-        return ""
