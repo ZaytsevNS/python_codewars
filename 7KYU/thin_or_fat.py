7b192c0f83ce085d5928ffebdef379448a0c7190
ZaytsevNS <fatalbest@mail.ru>
2023-04-28 19:51:57 +0300

    Solution for task: Matrix Weight.

---
+++ 7KYU/thin_or_fat.py
@@ -0,0 +1,13 @@
+# Solution for task: https://www.codewars.com/kata/6347f9715467f0001b434936/
+
+from math import sqrt
+from typing import Optional
+
+
+def thin_or_fat(matrix: list) -> Optional[str]:
+    try:
+        sum_widths = sum([sqrt(sum(i)) for i in matrix])
+        sum_height = sum([sqrt(sum(i)) for i in zip(*matrix)])
+        return 'thin' if sum_widths < sum_height  else ('fat' if sum_widths > sum_height else 'perfect')
+    except ValueError:
+        return None
