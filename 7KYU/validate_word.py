ffec7c2613b1b49ea1dc2d416529683febf0b7b0
Nickolay Zaytsev <fatalbest@mail.ru>
2023-10-15 11:21:26 +0300

    Fix SyntaxError.

--- 7KYU/validate_word.py
+++ 7KYU/validate_word.py
@@ -2,5 +2,5 @@ from collections import Counter
 
 
 def validate_word(s: str) -> bool:
-    count = Counter(s.lower())
-    return True if len(set(count.values())) < 2 else False
+    count = Counter(s.lower())
+    return True if len(set(count.values())) < 2 else False
